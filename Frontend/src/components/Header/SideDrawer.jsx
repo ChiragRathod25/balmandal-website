16c3177746f0062ad6dcde5e8c888ce04b24595f
Chirag Rathod <chiragrathod9987@gmail.com>
2025-02-24 23:59:26 +0530

    Style updates for BottomTabBar, SideDrawer, and TopBar components: change background colors, improve hover effects, and enhance text visibility.

--- Frontend/src/components/Header/SideDrawer.jsx
+++ Frontend/src/components/Header/SideDrawer.jsx
@@ -1,11 +1,12 @@
-import { X } from 'lucide-react';
-import { motion } from 'framer-motion';
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import databaseService from '../../services/database.services';
+import { X } from "lucide-react";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import databaseService from "../../services/database.services";
+import { Link } from "react-router-dom";
 
 const SideDrawer = ({ isOpen, onClose }) => {
   const [user, setUser] = useState(null);
+
   useEffect(() => {
     if (!user) {
       databaseService.getCurrentuser().then((res) => {
@@ -13,34 +14,31 @@ const SideDrawer = ({ isOpen, onClose }) => {
       });
     }
   }, []);
-  useEffect(() => {
-    if (user) {
-      console.log('user', user);
-    }
-  }, [user]);
+
   return (
     <>
       {/* Backdrop */}
       {isOpen && (
-        <div className="fixed inset-0 bg-transparent bg-opacity-50 z-40" onClick={onClose} />
+        <div
+          className="fixed inset-0 bg-transparent bg-opacity-50 z-40"
+          onClick={onClose}
+        />
       )}
 
       {/* Sliding Drawer */}
       <motion.div
-        initial={{ x: '-100%' }}
-        animate={{ x: isOpen ? 0 : '-100%' }}
+        initial={{ x: "-100%" }}
+        animate={{ x: isOpen ? 0 : "-100%" }}
         transition={{ duration: 0.3 }}
-        className="fixed top-0 left-0 w-64 h-full bg-white shadow-lg z-50 p-4"
+        className="fixed top-0 left-0 w-64 h-full bg-[#C30E59] text-white shadow-lg z-50 p-4"
       >
         {/* Close Button */}
-        <button onClick={onClose} className="mb-4">
+        <button onClick={onClose} className="mb-4 hover:text-[#F2AE66]">
           <X className="w-6 h-6" />
         </button>
-        {/* Profile  */}
-        <div className="flex items-center gap-4 mb-4 
-        border-b border-gray-200 pb-4
-        flex-col
-        "> 
+
+        {/* Profile */}
+        <div className="flex items-center gap-4 mb-4 border-b border-[#F2AE66] pb-4 flex-col">
           <img
             src={
               user?.avatar ||
@@ -49,28 +47,27 @@ const SideDrawer = ({ isOpen, onClose }) => {
             alt={user?.firstName}
             className="w-12 h-12 rounded-full"
           />
-          <div>
+          <div className="text-center">
             <h2 className="text-lg font-semibold">
               {user?.firstName} {user?.lastName}
             </h2>
-            <p className="text-sm text-gray-500"></p>
           </div>
         </div>
 
         {/* Menu Items */}
         <nav className="flex flex-col gap-4">
-          <a href="/" className="text-lg">
+          <Link to="/" className="hover:text-[#F2AE66] text-lg">
             Home
-          </a>
-          <a href="/event" className="text-lg">
+          </Link>
+          <Link to="/event" className="hover:text-[#F2AE66] text-lg">
             Events
-          </a>
-          <a href="/profile" className="text-lg">
+          </Link>
+          <Link to="/profile" className="hover:text-[#F2AE66] text-lg">
             Profile
-          </a>
-          <a href="/logout" className="text-lg">
+          </Link>
+          <Link to="/logout" className="hover:text-[#F2AE66] text-lg">
             Logout
-          </a>
+          </Link>
         </nav>
       </motion.div>
     </>
