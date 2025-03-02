7323752f19e065f16d9c8020e56acd5744aa63ba
Chirag Rathod <chiragrathod9987@gmail.com>
2025-03-02 14:49:37 +0530

    Fix import statement in post controller and export comment model correctly

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -2,7 +2,7 @@ import { ApiResponce } from "../utils/ApiResponce.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { Post } from "../models/post.model";
+import { Post } from "../models/post.model.js";
 
 //controllers list
 //1. addPost
--- Backend/src/models/comment.model.js
+++ Backend/src/models/comment.model.js
@@ -21,4 +21,4 @@ const commentSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Comment = mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model("Comment", commentSchema);
